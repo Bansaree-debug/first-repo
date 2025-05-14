e798e155ae77cba9b5e873e1b90cd6249d93018e
BANSAREE BHALALA <BANSAREE.BHALALA@einfochips.com>
2025-05-14 12:15:00 +0530

    hi

---
+++ 166690_Dennis_Ritchie_ch1_q1_T005.c
@@ -0,0 +1,10 @@
+//How would you test the word count program? What kinds of input are most likely to uncover bugs if there are any?
+//Author = Bansaree
+//Created = 26/12/2024
+
+#include<stdio.h>
+int main()
+{
+	printf("hello,world\n");
+}
+
